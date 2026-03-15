b53356a0351f0985dd5408f5eb58172ff7bb9fec
MIHMahmudEli <mdmohsin11222@gmail.com>
2026-03-16 00:27:06 +0600

    Fix Utils file upload problem

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import string
+from storage import StorageManager
 class Utils:
     @staticmethod
     def get_password_strength(password):
@@ -14,3 +15,18 @@ class Utils:
         if any(char in string.punctuation for char in password):
             strength += 1
         return strength
+
+    @staticmethod
+    def is_duplicate(website, username):
+        data = StorageManager().load()
+        for item in data:
+            if item.get("website") == website and item.get("username") == username:
+                return True
+        return False
+
+class PreventEmptyInput:
+    def __init__(self, message):
+        self.message = message
+    def __call__(self, value):
+        if not value:
+            raise ValueError(self.message)
